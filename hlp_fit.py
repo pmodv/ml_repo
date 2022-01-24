92ccfa2095fe783f225c83a68f92ccfff197e4ee
pmodv <pvernes@gmail.com>
2022-01-24 04:16:05 -0500

    Update hlp_fit.py
    updated model to only LR & std scaler

--- hlp_fit.py
+++ hlp_fit.py
@@ -2,6 +2,8 @@
 import hlp_eda as eda1
 import sklearn as sk
 
+import numpy as np
+
 from imblearn.ensemble import EasyEnsembleClassifier
 
 from sklearn.linear_model import LogisticRegression
@@ -11,7 +13,7 @@ from sklearn.model_selection import GridSearchCV
 
 
 
-from sklearn.decomposition import PCA, NMF
+from sklearn.decomposition import PCA, KernelPCA
 
 from numpy import mean
 
@@ -31,6 +33,8 @@ from category_encoders import TargetEncoder
 
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+
+
 
 # y_value is HIGH_RISK
 df_y = eda1.df_risk['HIGH_RISK']
@@ -55,7 +59,9 @@ print(df_y.shape)
 
 l_col_tgt_encode = ['NAME_INCOME_TYPE','NAME_FAMILY_STATUS','NAME_HOUSING_TYPE']
 
-l_col_bin_encode = ['CODE_GENDER','FLAG_OWN_CAR','FLAG_OWN_REALTY']
+l_col_bin_encode = ['CODE_GENDER','FLAG_OWN_CAR','FLAG_OWN_REALTY','FLAG_MOBIL','FLAG_WORK_PHONE','FLAG_EMAIL']
+
+l_num_col = ['CNT_CHILDREN','AMT_INCOME_TOTAL','DAYS_BIRTH','DAYS_EMPLOYED','CNT_FAM_MEMBERS']
 
 # implied order, here - works
 l_col_ord_encode = ['NAME_EDUCATION_TYPE']
@@ -63,13 +69,15 @@ l_col_ord_encode = ['NAME_EDUCATION_TYPE']
 preprocessor = ColumnTransformer(
     transformers=[('ordinal', ce.OrdinalEncoder(), l_col_ord_encode),
                   ('targetbased', ce.LeaveOneOutEncoder(), l_col_tgt_encode),
-                  ('ohe', ce.OneHotEncoder(), l_col_bin_encode)
+                  ('ohe', ce.OneHotEncoder(), l_col_bin_encode),
+                  ('scaler',StandardScaler(), l_num_col)
                  ], remainder='passthrough')
 
-#print(preprocessor.fit_transform(df_X_final,df_y_final))
-model = EasyEnsembleClassifier()
 
-pipe = Pipeline(steps=[('preprocessor', preprocessor), ('reduce_dim', 'passthrough'),('scaler', 'passthrough'),('classifier', model)])
+#print(preprocessor.fit_transform(df_X_final,df_y_final))
+model = LogisticRegression()
+
+pipe = Pipeline(steps=[('preprocessor', preprocessor), ('reduce_dim', 'passthrough'),('classifier', model)])
 
 
 N_FEATURES_OPTIONS = [2, 4, 8]
@@ -78,34 +86,35 @@ C_OPTIONS = [1, 10, 100, 1000]
 parameters = [
     {
         # doesn't make sense to use linear decomposition on RF, and so just use it, here
-        "reduce_dim": [PCA(iterated_power=7), NMF()],
+        "reduce_dim": [PCA(iterated_power=7)],
         "reduce_dim__n_components": N_FEATURES_OPTIONS,
 
-        # DOES make sense to standardize numeric values, though
-        "scaler": [StandardScaler()],
-
         'classifier': (LogisticRegression(class_weight='balanced'),),
-        'classifier__C': (0.001,0.01,0.1,1,10,100)
-    }, {
-        # no sense scaling for RF
-        "scaler": ['passthrough'],
-        'classifier': (EasyEnsembleClassifier(),),
-        'classifier__n_estimators': (10, 30,50)
-    }
-]
-# step 1: compare dim reduction of RF (non-linear) vs PCA (linear)
+        'classifier__C': (0.001,0.01,0.1,1)
+    }, 
+    {
+        # nonlinear dim reduction, too
+        "reduce_dim": [KernelPCA()],
+        "reduce_dim__gamma": np.linspace(0.03, 0.05, 10),
+        "reduce_dim__kernel": ["rbf", "sigmoid"],
 
-# need to oversample minority RF - will do this by including ALL minority class(HIGH_RISK) and just bootstrapping majority class
-
-
+        
+        'classifier': (LogisticRegression(class_weight='balanced'),),
+        'classifier__C': (0.001,0.01,0.1,1)
+    }]
+    
+ 
 cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 
 scoring = {"AUC": "roc_auc", "Accuracy": make_scorer(accuracy_score)}
 
-gs = GridSearchCV(pipe,param_grid=parameters, n_jobs=-1,cv=cv,scoring=scoring, refit="AUC")
+gs = GridSearchCV(pipe,param_grid=parameters,cv=cv,scoring=scoring, refit="AUC")
 #scores = cross_val_score(pipe, parameters, df_X_final, df_y_final, scoring='roc_auc', cv=cv, n_jobs=-1)
 
 result = gs.fit(df_X_final,df_y_final)
 
 print('Best Score: %s' % result.best_score_)
 print('Best hyperparameters: %s ' % result.best_params_)
+
+
+
